52415c4f953b59c2c6ae67974359417559a13c48
April John Maraat <amaraat@corebridge.net>
2020-03-13 11:14:30 +0800

    Added api response template.

---
+++ Riva.Api/Models/Responses/ResponseMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Riva.Api.Models.Responses
+{
+    public enum ResponseMessage
+    {
+        /// <summary>
+        /// Response message type for success
+        /// </summary>
+        Success,
+        /// <summary>
+        /// Response message type for exception
+        /// </summary>
+        Exception,
+        /// <summary>
+        /// Response message type for miscellaneous error
+        /// </summary>
+        MiscError
+    }
+}
---
+++ Riva.Api/Models/Responses/ResponseState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Riva.Api.Models.Responses
+{
+    public enum ResponseState
+    {
+        /// <summary>
+        /// Success response state
+        /// </summary>
+        Success,
+        /// <summary>
+        /// Error response state
+        /// </summary>
+        Error,
+        /// <summary>
+        /// Exception response state
+        /// </summary>
+        Exception
+    }
+}
